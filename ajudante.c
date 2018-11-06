d3f976c47d5af95867d4042d5de16b17939b6123
pedroLucas25 <pedrolucas111193@hotmail.com>
2018-11-06 09:26:37 -0300

    Testes

--- ajudante.c
+++ ajudante.c
@@ -2,25 +2,40 @@
 #include <stdlib.h>
 #include <time.h>
 #include <pthread.h>
+#include <unistd.h>
+
+void estado_ajudante(void* Param_alunos);
+void estado_alunos(void* Param_ajudante);
 
 typedef struct{
   int cont_ajudas;
+  int pos_cadeira;
 }Param_alunos;
 
+typedef struct{
+  int o_cadeira;
+}Param_ajudante;
 
-int main () {
+int main(){
 
-   time_t t;
-   srand((unsigned) time(&t));
+  time_t t;
+  srand((unsigned) time(&t));
 
-   int num_alunos, num_cadeiras;
+  int num_alunos, num_cadeiras;
 
-   num_alunos = (rand() % 38) + 3;
-   num_cadeiras = num_alunos/2;
+  num_alunos = (rand() % 38) + 3;
+  num_cadeiras = num_alunos/2;
 
-   pthread_t dthread_alunos[num_alunos];
-   pthread_t dthread_ajudante;
+  pthread_t dthread_alunos[num_alunos];
+  pthread_t dthread_ajudante;
 
+  return(0);
+}
 
-   return(0);
+void estado_alunos(void* Param_alunos){
+
+}
+
+void estado_ajudante(void* Param_ajudante){
+
 }
